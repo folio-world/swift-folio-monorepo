47dd8d7d298e9a79ec0cea1bc31de7a4ee0603fb
송영모 <mooyoung2309@gmail.com>
2023-10-02 13:09:29 +0900

    refactor: 폴더 위계 정리

---
+++ Projects/Toolinder/Feature/Trade/Interface/Sources/SelectTicker/SelectTickerStore.swift
@@ -0,0 +1,8 @@
+//
+//  SelectTickerStore.swift
+//  ToolinderFeatureTradeInterface
+//
+//  Created by 송영모 on 2023/10/02.
+//
+
+import Foundation
---
+++ Projects/Toolinder/Feature/Trade/Interface/Sources/SelectTicker/SelectTickerView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectTicker.swift
+//  ToolinderFeatureTradeInterface
+//
+//  Created by 송영모 on 2023/10/02.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+public struct SelectTickerView: View {
+    public let store: StoreOf<SelectTagStore>
+    
+    public var body: some View {
+        HStack {
+            
+        }
+    }
+}
